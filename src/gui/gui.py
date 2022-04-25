d6d42969fc8777f4a61b1f7609571178205bd05b
ireichard <ireichard4@gmail.com>
2022-04-25 15:07:10 -0700

    Tiled the windows in a grid, added layout documentation in docstring

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,6 +1,17 @@
 """
 Scion's GUI Application (name is pending)
 
+Layout:
+(Not to scale)
+-------------------------------------------------
+Top Bar                    |          | Sensors |
+---------------------------- Camera 1 |         |
+Buttons | Status | Weapons |----------|         |
+---------------------------- Camera 2 |         |
+Logging                    |          |         |
+-------------------------------------------------
+
+
 Latest verification test:
 3/16/22 on Linux - IAR
 3/17/22 on Windows - IAR
@@ -33,7 +44,7 @@ import cv2
 edge_size = 2
 # Resolution height = top bar + video1 + edge + video2
 gui_resolution = (1600, 960)
-camera_resolution = (640, 480)
+camera_resolution = (640, 480)  # Both cameras will use 480p
 top_bar_resolution = (640, 30)
 button_display_resolution = (100, 240)
 weapons_status_resolution = (270, 240)
@@ -54,13 +65,26 @@ class GuiWindow(tk.Frame):
         self.tk_master = master
         self.resolution = gui_resolution
         tk.Frame.__init__(self, self.tk_master)
-        self.top_bar = tk.Frame(master=self.tk_master, width=self.resolution[0], height=top_bar_resolution[1], bg='white')
+        self.top_bar = tk.Frame(master=self.tk_master, width=top_bar_resolution[0], height=top_bar_resolution[1], bg='black')
+        self.button_display = tk.Frame(master=self.tk_master, width=button_display_resolution[0], height=button_display_resolution[1], bg='blue')
+        self.weapons_status = tk.Frame(master=self.tk_master, width=weapons_status_resolution[0], height=weapons_status_resolution[1], bg='red')
+        self.thruster_status = tk.Frame(master=self.tk_master, width=thruster_status_resolution[0], height=thruster_status_resolution[1], bg='green')
+        self.logging = tk.Frame(master=self.tk_master, width=logging_resolution[0], height=logging_resolution[1], bg='grey')
+        self.camera = tk.Frame(master=self.tk_master, width=camera_resolution[0], height=camera_resolution[1]*2, bg='orange')
+        self.sensors = tk.Frame(master=self.tk_master, width=sensor_resolution[0], height=sensor_resolution[1], bg='yellow')
+        # Main grid
+        self.top_bar.grid(row=0, column=0, columnspan=3)
+        self.button_display.grid(row=1, column=0)
+        self.weapons_status.grid(row=1, column=1)
+        self.thruster_status.grid(row=1, column=2)
+        self.logging.grid(row=2, column=0, columnspan=3)
+        self.camera.grid(row=0, column=3, rowspan=3)
+        self.sensors.grid(row=0, column=4, rowspan=3)
 
-
-def init_gui() -> None:
+def init_gui(host_os: str) -> None:
     """Starts up GUI window and all related programs
     """
-    if os.name == 'nt':  # Windows
+    if host_os == 'nt':  # Windows
         context = mp.get_context('spawn')
     else:  # Linux
         context = mp.get_context('fork')
@@ -73,10 +97,9 @@ def init_gui() -> None:
 
 
 if __name__ == '__main__':
-    n = os.name
-    if n == 'nt':  # Windows
+    if os.name == 'nt':  # Windows
         mp.set_start_method('spawn')
     else:  # Linux
         mp.set_start_method('fork')
-    print(f'{__name__} started on {n} at {os.getpid()}')  # Kept for testing
-    init_gui()
+    print(f'{__name__} started on {os.name} at {os.getpid()}')  # Kept for testing
+    init_gui(host_os=os.name)
