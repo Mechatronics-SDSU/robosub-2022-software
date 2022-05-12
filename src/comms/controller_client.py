90906baddeb2776b93dc968f394badaff509cf15
Ian <ireichard4@gmail.com>
2022-05-11 17:11:14 -0700

    Cleanup for controller client, now requires arguments

--- src/comms/controller_client.py
+++ src/comms/controller_client.py
@@ -1,4 +1,8 @@
-import math
+"""Controller client for sending controller inputs to remote server (Scion).
+Ported from Pico for the 5-7-22 initial test, worked well, keeping it with minor changes.
+"""
+
+import sys
 import numpy as np
 import control.controller_translator as ct
 import pygame as pg
@@ -6,11 +10,19 @@ import socket
 import struct
 
 
-def pilot_proc() -> None:
+def pilot_proc(argc: int, argv: list) -> None:
     """Sends controller input to Pico's Control over TCP connection.
     """
-    hostname = '192.168.3.1'
-    port = 50004
+    hostname = None
+    port = None
+    if argc > 2:  # Did we get a host?
+        hostname = argv[1].replace(' ', '')
+        port = argv[2].replace(' ', '')
+    else:
+        print(f'Error: Expected argc > 2, number of args = {argc}. (Did you add the server\'s IP address/port?)')
+        print('Exiting Controller Client...')
+        sys.exit()
+
     last_input = np.zeros(shape=(1, 1))
     started = False
     server_conn = False
@@ -34,21 +46,19 @@ def pilot_proc() -> None:
             server_conn = False
             if rcon_try_count >= rcon_try_counter_max:
                 started = False
-            print(f'Connection to server failed... Reconnect attempt {rcon_try_count}/{rcon_try_counter_max}')
+            print(f'Connection to server @ {hostname}:{port} failed... Reconnect attempt {rcon_try_count}/\
+                    {rcon_try_counter_max}')
         while server_conn:
             try:
                 data = s.recv(1024)
             except (ConnectionAbortedError, ConnectionResetError) as e:
-                # pilot_pipe_out.send(('gui', 'pilot', 'no_conn_socket'))
                 print(f'Connection to server lost...')
                 server_conn = False
                 break
             if data == b'1':
                 data = None
                 if js.get_init():
-                    control_in = np.zeros(shape=(1, js.get_numaxes()
-                                                + js.get_numbuttons()
-                                                + js.get_numhats()))
+                    control_in = np.zeros(shape=(1, js.get_numaxes() + js.get_numbuttons() + js.get_numhats()))
                     for i in range(js.get_numaxes()):
                         control_in.put(i, js.get_axis(i))
                     for i in range(js.get_numaxes(), js.get_numbuttons()):  # Buttons
@@ -56,18 +66,10 @@ def pilot_proc() -> None:
 
                     control_in.put((js.get_numaxes() + js.get_numbuttons()), js.get_hat(0))  # Hat
                     result = ctc.translate_to_maestro_controller(control_in)
-                    # print(f'Result: {result}')
                 pg.event.pump()
                 if len(result) > 0:
-                    last_input = struct.pack('>8b',
-                                                  result[0],
-                                                  result[1],
-                                                  result[2],
-                                                  result[3],
-                                                  result[4],
-                                                  result[5],
-                                                  result[6],
-                                                  result[7])
+                    last_input = struct.pack('>8b', result[0], result[1], result[2], result[3], result[4], result[5],
+                                             result[6], result[7])
                     try:
                         s.sendall(last_input)
                     except (ConnectionAbortedError, ConnectionResetError) as e:
@@ -87,9 +89,7 @@ def pilot_proc() -> None:
                 server_conn = False
                 break
             if js.get_init():
-                control_in = np.zeros(shape=(1, js.get_numaxes()
-                                            + js.get_numbuttons()
-                                            + js.get_numhats()))
+                control_in = np.zeros(shape=(1, js.get_numaxes() + js.get_numbuttons() + js.get_numhats()))
                 for i in range(js.get_numaxes()):
                     control_in.put(i, js.get_axis(i))
                 for i in range(js.get_numaxes(), js.get_numbuttons()):  # Buttons
@@ -102,35 +102,6 @@ def pilot_proc() -> None:
                 result.clear()
 
 
-def run_client() -> None:
-    """Test code using controller inputs directly. Don't run in other modules!"""
-    pg.init()
-    pg.joystick.init()
-    js = pg.joystick.Joystick(0)
-    js.init()
-    print(str(js.get_numaxes()) + ' ' + str(js.get_numbuttons()) + ' ' + str(js.get_numhats()))
-    ctc = ct.ControllerTranslator(joystick_drift_compensation=0.1, base_net_turn=10, base_net_strafe=-20, debug=True)
-    print('Started Controller Client, sending input')
-    while True:
-        if js.get_init():
-            control_in = np.zeros(shape=(1, js.get_numaxes()
-                                         + js.get_numbuttons()
-                                         + js.get_numhats()))
-            for i in range(js.get_numaxes()):
-                control_in.put(i, js.get_axis(i))
-            for i in range(js.get_numaxes(), js.get_numbuttons()):  # Buttons
-                control_in.put(i, js.get_button(i - js.get_numaxes()))
-
-            control_in.put((js.get_numaxes() + js.get_numbuttons()), js.get_hat(0))  # Hat
-            result = ctc.translate_to_maestro_controller(control_in)
-            print(f"PFZ: {result[0]} SFZ: {result[1]} SAZ: {result[2]} PAZ: {result[3]} PFV: {result[4]} "
-                  f"PAV: {result[5]} SFV: {result[6]} SAV: {result[7]}")
-        pg.event.pump()
-"""
-SOFTWARE: PFZ	SFZ	SAZ	PAZ	PFV	PAV	SFV	SAV
-
-"""
 if __name__ == '__main__':
     print('Starting Controller Client...')
-    pilot_proc()
-    #run_client()
+    pilot_proc(argc=len(sys.argv), argv=sys.argv)
