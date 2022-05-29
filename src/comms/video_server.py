e7c16e1174aefa41523018d7fc269f334443d75d
ireichard <ireichard4@gmail.com>
2022-05-29 02:22:23 -0700

    Tests are failing because of OpenCV bs will need to look back at it later

--- src/comms/video_server.py
+++ src/comms/video_server.py
@@ -31,15 +31,15 @@ def main(host: str, port: int, ind: bool, write_pipe=None, context=None, camera_
     # Display frames and print
     while True:
         while len(data) < payload_size:
-            print("Recv: {}".format(len(data)))
+            # print("Recv: {}".format(len(data)))
             data += conn.recv(4096)
         if shm_camera.buf[0] == 1:
             shm_camera.buf[0] = 2  # We're now receiving frames
-        print("Done Recv: {}".format(len(data)))
+        # print("Done Recv: {}".format(len(data)))
         packed_msg_size = data[:payload_size]
         data = data[payload_size:]
         msg_size = struct.unpack(">L", packed_msg_size)[0]
-        print("msg_size: {}".format(msg_size))
+        # print("msg_size: {}".format(msg_size))
         while len(data) < msg_size:
             data += conn.recv(4096)
         frame_data = data[:msg_size]
