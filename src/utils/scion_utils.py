ca47c7ef4948645e800a85aeff0124fbbb19ea35
Ian <ireichard4@gmail.com>
2022-06-03 02:47:36 -0700

    ROS logger in utils (untested)

---
+++ src/utils/scion_utils.py
@@ -0,0 +1,18 @@
+import sys
+
+ROS_MODULE_NAME = 'rospy'
+
+
+def scion_print(print_input: str):
+    """Prints to ROS if rospy is imported.
+    """
+    if ROS_MODULE_NAME not in sys.modules:
+        print(f'{ROS_MODULE_NAME} Not imported.')
+    else:
+        pub = rospy.Publisher('logging', String, queue_size=1)
+        pub.publish(print_input)
+
+
+if __name__ == '__main__':
+    print('Do not ruin scion_utils as main!')
+    sys.exit(1)
