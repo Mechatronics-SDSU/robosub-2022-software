2cb53b81f7c93ca0f6c2c0d158ee77ebc7ded1f7
ireichard <ireichard4@gmail.com>
2022-06-03 23:57:47 -0700

    Setting up shared memory in telemetry linker

--- src/sensor/telemetry_linker.py
+++ src/sensor/telemetry_linker.py
@@ -37,6 +37,43 @@ class TelemetryLinker:
     """
     def __init__(self):
         self.data = copy.deepcopy(telemetry_data)
+        self.ahrs_pitch_shm = None
+        self.ahrs_roll_shm = None
+        self.ahrs_yaw_shm = None
+        self.depth_shm = None
+        self.setup()
+
+    def setup(self):
+        """Set up all shared memory
+        """
+        # Pitch
+        try:
+            self.ahrs_pitch_shm = shm.SharedMemory(name='ahrs_pitch_shm')
+        except FileNotFoundError:
+            self.ahrs_pitch_shm = shm.SharedMemory(create=True, size=8, name='ahrs_pitch_shm')
+        for i in range(self.ahrs_pitch_shm.size):
+            self.ahrs_pitch_shm.buf[i] = 0
+        # Roll
+        try:
+            self.ahrs_roll_shm = shm.SharedMemory(name='ahrs_roll_shm')
+        except FileNotFoundError:
+            self.ahrs_roll_shm = shm.SharedMemory(create=True, size=8, name='ahrs_roll_shm')
+        for i in range(self.ahrs_roll_shm.size):
+            self.ahrs_roll_shm.buf[i] = 0
+        # Yaw
+        try:
+            self.ahrs_yaw_shm = shm.SharedMemory(name='ahrs_yaw_shm')
+        except FileNotFoundError:
+            self.ahrs_yaw_shm = shm.SharedMemory(create=True, size=8, name='ahrs_yaw_shm')
+        for i in range(self.ahrs_yaw_shm.size):
+            self.ahrs_yaw_shm.buf[i] = 0
+        # Depth
+        try:
+            self.depth_shm = shm.SharedMemory(name='depth_sensor_shm')
+        except FileNotFoundError:
+            self.depth_shm = shm.SharedMemory(create=True, size=8, name='depth_sensor_shm')
+        for i in range(self.depth_shm.size):
+            self.depth_shm.buf[i] = 0
 
     def load_shm(self, shm_val: int, value: any) -> None:
         """Load shared memory located at shm_val with MSB to LSB byte-ordered values in list.
@@ -46,6 +83,8 @@ class TelemetryLinker:
     def load_all(self) -> None:
         """Load shared memory, REGARDLESS OF IF DATA HAS BEEN PROPERLY UPDATED ELSEWHERE, into self.data.
         """
+        for i in self.data.keys():
+            print(i)
 
     def get_shm(self, shm_val: int) -> any:
         """Get what is stored at shm_val with MSB to LSB byte-ordered values in list.
@@ -60,12 +99,13 @@ class TelemetryLinker:
     def unpack_shm(self) -> any:
         """Unpack a pickle with all data.
         """
+        pass
 
 
 def telemetry_server():
     """Start a socket server for GUI to access for telemetry data.
     """
-    pass
+    tel = TelemetryLinker()
 
 
 if __name__ == '__main__':
