2a30a818d679202a15c29358870ecb92a4a68013
ireichard <ireichard4@gmail.com>
2022-06-15 11:47:50 -0700

    Cleanup

--- src/start/thruster_sys.py
+++
@@ -1,1 +0,0 @@
-print('Thruster System')
--- src/tests/dev/maestro.sh
+++ src/tests/dev/maestro.sh
@@ -2,7 +2,7 @@
 MAESTRO=$(ls /dev | grep ttyACM | wc -l)
 if [[ $MAESTRO > 0 ]]; then
   echo "Maestro Device found at:"
-  ls /dev | grep ttyACM | python3 start/maestro_dev_test.py
+  ls /dev | grep ttyACM
 else
   echo "No maestro device detected"
 fi
