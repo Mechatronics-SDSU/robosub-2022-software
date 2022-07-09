35f6ab536979c72f954c2638e9378c6c1e61dc66
ireichard <ireichard4@gmail.com>
2022-07-08 18:06:32 -0700

    AIO board working

--- src/aio/aio_linker.py
+++ src/aio/aio_linker.py
@@ -2,7 +2,7 @@
 """AIO linker for GUI and listener.
 """
 import copy
-from multiprocessing import shared_memory as shm
+import pickle
 
 AIO_DEFAULT_DATA = (0,  # Kill switch
                     0,  # Auto switch
@@ -36,30 +36,55 @@ aio_vals_from_name = {
     'arm': 5
 }
 
+aio_vals_from_char = {
+    'k': 0,
+    'a': 1,
+    'b': 2,
+    'l': 3,
+    'w': 4,
+    'r': 5
+}
+
+aio_chars_from_val = {
+    0: 'k',
+    1: 'a',
+    2: 'b',
+    3: 'l',
+    4: 'w',
+    5: 'r'
+}
+
 aio_data = []
 for x in range(len(AIO_DEFAULT_DATA)):
     aio_data.append(AIO_DEFAULT_DATA[x])
 
 
 class AIOLinker:
-    def __init__(self, use_shm=True):
+    def __init__(self):
         self.data = copy.deepcopy(aio_data)
-        if use_shm:
-            self.shm_objects = []
-            self.setup_shm()
 
-    def setup_shm(self):
-        """Set up all shared memory
+    def set_current_state(self, system: str, state: int) -> None:
+        sys_index = aio_vals_from_char[system]
+        self.data[sys_index] = state
+
+    def serialize(self) -> bytes:
+        return pickle.dumps(self.data)
+
+    def from_serial(self, input_data: bytes) -> None:
+        if input_data != b'':
+            self.data = pickle.loads(input_data)
+
+    def from_serial_with_diff(self, input_data: bytes) -> list:
+        """Serializes and returns 2 lists of what was changed.
         """
+        if input_data != b'':
+            in_data = pickle.loads(input_data)
+        else:
+            return [[], []]
+        out = [[], []]  # Output is a tuple of ros characters and entries
         for i in range(len(self.data)):
-            try:
-                shm_i = shm.SharedMemory(name=f'aio_{aio_names_from_val[i]}_shm')
-            except FileNotFoundError:
-                shm_i = shm.SharedMemory(create=True, size=AIO_DATA_SIZES[i],
-                                         name=f'aio_{aio_names_from_val[i]}_shm')
-            for j in range(AIO_DATA_SIZES[i]):  # Clear any existing data
-                shm_i.buf[j] = 0
-            self.shm_objects.append(shm_i)
-
-
-
+            if in_data[i] != self.data[i]:
+                out[0].append(aio_chars_from_val[i])  # Character for ROS topic
+                out[1].append(in_data[i])  # Data entry
+        self.data = in_data
+        return out
--- src/catkin_ws/src/scion_ros/driver/aio_handler.py
+++ src/catkin_ws/src/scion_ros/driver/aio_handler.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.8
 import rospy
 from std_msgs.msg import String
 
@@ -15,25 +15,25 @@ def aio_handler(aio_name: str, debug=False) -> None:
     aio = scion_aio.AIOWrapper(device_name=aio_name, timeout=AIO_TIMEOUT_MS)
     # ROS topics
     aio_auto_pub = rospy.Publisher('aio_auto_state', String, queue_size=1)
-    aio_auto_dw = scion_ut.DataWrapper(debug=False)
+    aio_auto_dw = scion_ut.DataWrapper(debug=debug)
     rospy.Subscriber('aio_auto_change', String, aio_auto_dw.callback)
     aio_bat_pub = rospy.Publisher('aio_bat_state', String, queue_size=1)
-    aio_bat_dw = scion_ut.DataWrapper(debug=False)
+    aio_bat_dw = scion_ut.DataWrapper(debug=debug)
     rospy.Subscriber('aio_bat_change', String, aio_bat_dw.callback)
     aio_kill_pub = rospy.Publisher('aio_kill_state', String, queue_size=1)
-    aio_kill_dw = scion_ut.DataWrapper(debug=False)
+    aio_kill_dw = scion_ut.DataWrapper(debug=debug)
     rospy.Subscriber('aio_kill_change', String, aio_kill_dw.callback)
     aio_leak_pub = rospy.Publisher('aio_leak_state', String, queue_size=1)
-    aio_leak_dw = scion_ut.DataWrapper(debug=False)
+    aio_leak_dw = scion_ut.DataWrapper(debug=debug)
     rospy.Subscriber('aio_leak_change', String, aio_leak_dw.callback)
     aio_torp_pub = rospy.Publisher('aio_torp_state', String, queue_size=1)
-    aio_torp_dw = scion_ut.DataWrapper(debug=False)
+    aio_torp_dw = scion_ut.DataWrapper(debug=debug)
     rospy.Subscriber('aio_torp_change', String, aio_torp_dw.callback)
     aio_arm_pub = rospy.Publisher('aio_arm_state', String, queue_size=1)
-    aio_arm_dw = scion_ut.DataWrapper(debug=False)
+    aio_arm_dw = scion_ut.DataWrapper(debug=debug)
     rospy.Subscriber('aio_arm_change', String, aio_arm_dw.callback)
     rospy.init_node('aio_handler', anonymous=True)
-    rate = rospy.Rate()
+    rate = rospy.Rate(AIO_FETCH_DELAY)
     # Main loop
     while True:
         # Check for new messages
@@ -53,7 +53,7 @@ def aio_handler(aio_name: str, debug=False) -> None:
                 aio_torp_pub.publish(ret)
             elif ret[1] == scion_ut.AIO_ARM_NMASK_ROS:
                 aio_arm_pub.publish(ret)
-        rate.sleep(AIO_FETCH_DELAY)
+        rate.sleep()
         # Check for latest messages in callbacks
         '''
         Order of the following if/else block matters. Things ordered higher in the block
@@ -72,7 +72,7 @@ def aio_handler(aio_name: str, debug=False) -> None:
                 print(f'AIO kill sees: callback_data={data} | nmask={nmask} | val={val}')
             aio.send_input_packet(nmask=nmask, value=val)
             aio_kill_dw.data = None
-        elif aio_leak_dw is not None:
+        elif aio_leak_dw.data is not None:
             data = str(aio_leak_dw.data)
             nmask = data[1]
             val = int(data[2], 16)
@@ -96,7 +96,7 @@ def aio_handler(aio_name: str, debug=False) -> None:
                 print(f'AIO auto sees: callback_data={data} | nmask={nmask} | val={val}')
             aio.send_input_packet(nmask=nmask, value=val)
             aio_auto_dw.data = None
-        elif aio_torp_dw is not None:
+        elif aio_torp_dw.data is not None:
             data = str(aio_torp_dw.data)
             nmask = data[1]
             val = int(data[2], 16)
@@ -104,7 +104,7 @@ def aio_handler(aio_name: str, debug=False) -> None:
                 print(f'AIO torp sees: callback_data={data} | nmask={nmask} | val={val}')
             aio.send_input_packet(nmask=nmask, value=val)
             aio_torp_dw.data = None
-        elif aio_arm_dw is not None:
+        elif aio_arm_dw.data is not None:
             data = str(aio_arm_dw.data)
             nmask = data[1]
             val = int(data[2], 16)
@@ -112,7 +112,7 @@ def aio_handler(aio_name: str, debug=False) -> None:
                 print(f'AIO arm sees: callback_data={data} | nmask={nmask} | val={val}')
             aio.send_input_packet(nmask=nmask, value=val)
             aio_arm_dw.data = None
-        rate.sleep(AIO_FETCH_DELAY)
+        rate.sleep()
 
 
 if __name__ == '__main__':
