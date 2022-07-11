78ef8de338dd47d8804e0a9237acca6fad428b91
TjRichmond <tjrichmond99@gmail.com>
2022-07-10 19:05:25 -0700

    Updated mount name to fstring

--- src/aio/aio_serial_util.py
+++ src/aio/aio_serial_util.py
@@ -14,11 +14,11 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:  # Get USB mount location as an argument
         dev = sys.argv[1].replace(' ', '')
 
-        arduino = serial.Serial(port='{dev}', baudrate=9600, timeout=.1)
+        arduino = serial.Serial(port=f'{dev}', baudrate=9600, timeout=.1)
 
         while True:
             command = input("Enter a command in bytes: ")
-            value = write_read(b'i\xA0\n') # Hard coded message sent to AIO, currently set as "get kill status" 
+            value = write_read(b'i\x3F\n') # Hard coded message sent to AIO, currently set as "get kill status" 
             print(value)
     else:
         print('AIO Utils Error, sys.argv less than 2. (Did you add the device name?)')
