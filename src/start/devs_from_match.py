e9f1fd3ac29eb123c3096b5fba397171b4b6603a
ireichard <ireichard4@gmail.com>
2022-07-15 11:03:25 -0700

    DVL BUGFIX

--- src/start/devs_from_match.py
+++ src/start/devs_from_match.py
@@ -1,6 +1,7 @@
 
 import fileinput
 import re
+import os
 
 
 pat_1 = 'ttyUSB'
@@ -35,6 +36,15 @@ def match_from_port() -> None:
     devs = get_devs_from_file()
     written_devs = []
     f = open('start/current_devices.cfg', 'w')
+    f_1 = open('start/current_device_names.cfg', 'r')
+    f_1_entries = f_1.readlines()
+    f_1.close()
+    for name in f_1_entries:
+        f_1_entries[f_1_entries.index(name)] = f_1_entries[f_1_entries.index(name)].rstrip('\n')
+    ind = 0
+    dev_names = []
+    for i in range(len(devs)):
+        dev_names.append('')
     for line in fileinput.input():
         for dev in devs:
             line = line.strip()
@@ -45,10 +55,17 @@ def match_from_port() -> None:
                 test = re.search(pattern=pat_1, string=test)
                 if test is not None:
                     test = test.string
+                    dev_names[ind] = f_1_entries[devs.index(dev)]
                     parsed_test_str = parse_tty(test)
                     if parsed_test_str not in written_devs:
                         written_devs.append(parsed_test_str)
                         f.write(f'/dev/{parsed_test_str}\n')
+                        ind += 1
+    os.remove('start/current_device_names.cfg')
+    f_1 = open('start/current_device_names.cfg', 'w+')
+    for dev_name in dev_names:
+        f_1.write(f'{dev_name}\n')
+    f_1.close()
 
 
 if __name__ == '__main__':
