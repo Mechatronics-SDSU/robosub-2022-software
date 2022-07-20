e47edba185cdf55d37466698225314cc65824984
ireichard <ireichard4@gmail.com>
2022-07-19 17:49:22 -0700

    Heuristics BNF definitions

--- src/ai/heuristics.py
+++ src/ai/heuristics.py
@@ -2,10 +2,41 @@
 """
 import ai.mission_planner as scion_mis
 
+# Parser instructions for the mission planner.
+COMMENT = '!'
+COMPARE = '#'
+BREAK = '*'
+ITERABLE = '@'
+BEGIN_REGISTER = '{'
+END_REGISTER = '}'
+BEGIN_ROS_PARAM = '('
+END_ROS_PARAM = ')'
+BEGIN_VAR = "\""
+END_VAR = "\""
+PYTHON_FLOAT = 'f64'
+PYTHON_INT = 'i64'
+PYTHON_LIST = 'list'
+PYTHON_LIST_SEP = ','
+BEGIN_PYTHON_LIST = '['
+END_PYTHON_LIST = ']'
+COMP_EQ = '=='
+COMP_NE = '!='
+COMP_LE = '<='
+COMP_GE = '>='
+COMP_LT = '<<'
+COMP_GT = '>>'
+
 
 class Heuristics:
     def __init__(self, missions: str):
         self.missions = scion_mis.MissionSystem(load_from_file=missions)
+        self.instructions = self.parse_instructions()
+
+    def parse_instructions(self) -> list:
+        pass
+
+    def run_instruction(self, instruction: str) -> bool:
+        return True
 
 
 def heuristics_test():
